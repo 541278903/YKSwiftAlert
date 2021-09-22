b3ffa19f22f0d27e6d130b3ee890cebade585e00
edward <edward>
2021-09-22 19:44:02 +0800

    1

--- YKSwiftAlert/Classes/YKSwiftAlertCenter.swift
+++ YKSwiftAlert/Classes/YKSwiftAlertCenter.swift
@@ -23,13 +23,35 @@ class YKSwiftAlertCenter: NSObject {
             return
         }
         
+        // 删除旧的先
+//        UIView *beforeView = [view viewWithTag:[YKAlertCenter kYK_AlertMessage_Tag]];
+//        if (beforeView) {
+//            [beforeView removeFromSuperview];
+//            beforeView = nil;
+//        }
+        var beforeView = inView.viewWithTag(YKSwiftAlertCenter.kYK_AlertMessage_Tag())
+        if beforeView != nil {
+            beforeView!.removeFromSuperview()
+            beforeView = nil
+        }
+        
         let isPad:Bool = UI_USER_INTERFACE_IDIOM() == .pad
         let showW:CGFloat = (isPad) ? 200 : 150
         let showH:CGFloat = (isPad) ? 45 : 25
         let spacX:CGFloat = (isPad) ? 10 : 10
         
         let showFont:UIFont = UIFont.systemFont(ofSize: isPad ? 24 : 16)
-        let theStringSize:CGSize = message.
+        let nsMessage:NSString = NSString(string: message)
+        var theStringSize:CGSize = nsMessage.boundingRect(with: CGSize.init(width: showW-spacX*2, height: 1000), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font:showFont], context: nil).size
+        
+        if theStringSize.height<showH {
+            theStringSize.height = showH
+        }
+        
+        let showView:UIView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: showW, height: theStringSize.height+spacX*2))
+        showView.tag = YKSwiftAlertCenter.kYK_AlertMessage_Tag()
+        showView.alpha = 0
+        showView.backgroundColor = UIColor.black.withAlphaComponent(0)
     }
     
     public static func showLoading(message:String)->Void
@@ -40,5 +62,15 @@ class YKSwiftAlertCenter: NSObject {
     public static func dissLoading()->Void
     {
         
+    }
+    
+    private static func kYK_AlertMessage_Tag()->Int
+    {
+        return 889
+    }
+    
+    private static func kYK_AlertLoading_Tag()->Int
+    {
+        return 888
     }
 }
